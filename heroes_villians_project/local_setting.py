50a9537b45d4724cc5971492d58a859aa0322473
Raven Williams <heatherwilliams.raven@gmail.com>
2022-09-11 17:53:30 -0500

    Created a local_setting file in order to protect user information, password and secret key

---
+++ heroes_villians_project/local_setting.py
@@ -0,0 +1,13 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-c@i3te+gthjxr9t8onxvyzm=2l39@(^(d+4bl9tv4w&j8k3c9='
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroes_villians_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'HighlyFavored3124'
+    }
+}
--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c@i3te+gthjxr9t8onxvyzm=2l39@(^(d+4bl9tv4w&j8k3c9='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +72,7 @@ WSGI_APPLICATION = 'heroes_villians_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -121,3 +115,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villians_project.local_setting import *
+
+except ImportError:
+    pass
