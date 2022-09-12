83bb2f1a0c360306db8a0367a4191f1cea58a093
Raven Williams <heatherwilliams.raven@gmail.com>
2022-09-12 16:02:17 -0500

    added functionality to create additional supers through an http request instead of the admin site this will be for the users created a post request

--- supers/views.py
+++ supers/views.py
@@ -1,15 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
     
-    supers = Super.objects.all()
+    if request.method == 'GET':
 
-    serializer = SuperSerializer(supers, many=True)
-   
-    return Response(serializer.data)
+        supers = Super.objects.all()
+
+        serializer = SuperSerializer(supers, many=True)
+    
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
